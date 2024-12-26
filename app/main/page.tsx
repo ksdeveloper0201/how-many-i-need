773077f664a613ad6d0fc8d0e4a0461e65a49cfc
ksdeveloper0201 <ksdeveloper0201@gmail.com>
2024-12-27 07:52:59 +0900

    first init

---
+++ app/main/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+function mainPage() {
+    return (
+        <div>
+            <Button>test</Button>
+        </div>
+    );
+}
+
+export default mainPage;
